f8b416d987124cbbbcd9d0268537c0f76b50dc81
avespro <a.vesprini@eurolab-ap.it>
2023-03-15 16:22:38 +0100

    Create installcilium.sh

---
+++ installcilium.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+POD_CIDR="192.168.0.0/16"
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+CLI_ARCH="amd64"
+
+curl -sSL --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+
+cilium install --helm-set ipam.operator.clusterPoolIPv4PodCIDRList=$POD_CIDR
